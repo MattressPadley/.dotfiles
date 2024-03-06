817bb389f5f120e56c4a6b5232d6a15181b91626
Matt Hadley <matthadleyphoto@gmail.com>
2024-03-05 16:48:51 -0800

    app title in sketchybar

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -4,7 +4,11 @@
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+WINDOW_TITLE=$(/opt/homebrew/bin/yabai -m query --windows --window | jq -r '.title')
+APP_TITLE=$(/opt/homebrew/bin/yabai -m query --windows --window | jq -r '.app')
 
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set $NAME label="$INFO" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$INFO")"
-fi
+#if [ "$SENDER" = "front_app_switched" ]; then
+#  sketchybar --set $NAME label="$INFO | $WINDOW_TITLE" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$INFO")"
+#fi
+
+sketchybar --set $NAME label="$APP_TITLE | $WINDOW_TITLE" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$APP_TITLE")"
