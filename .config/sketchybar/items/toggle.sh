3cb7021a02a49cdc00c152d45beee8d56c685d35
Matt Hadley <hello@matthadley.me>
2025-05-05 06:50:37 -0700

    sophie update

---
+++ .config/sketchybar/items/toggle.sh
@@ -0,0 +1,5 @@
+sketchybar --add item toggler left \
+           --set toggler drawing=off \
+                         updates=on \
+                         script="$PLUGIN_DIR/toggle.sh" \
+           --subscribe toggler front_app_switched
